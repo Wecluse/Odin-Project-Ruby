ce108e8d5e78fb4e5d9b9916c75871a71c6e257d
Wecluse <152241855+Wecluse@users.noreply.github.com>
2024-10-17 20:10:05 +0800

    Added method

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,3 +1,10 @@
 def substrings(word, dictionary)
-    
+        result = Hash.new(0) # Initialize a hash to store the counts of found substrings
+        text = word.downcase   # Convert the input word to lowercase
+        dictionary.each do |sub| # Iterate over each substring in the provided dictionary
+            matches = text.scan(sub.downcase).size # size method returns the number of matches found
+            result[sub] += matches if matches > 0 # Increment count
+        end
+    end
+    result
 end
